2247a633d6bf6e0727130090047f46d5d2b5fe49
manuel927 <manuel.smo24@gmail.com>
2021-01-25 19:50:48 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/serviciosrestapi/PostService.java
@@ -0,0 +1,13 @@
+package co.edu.unipiloto.serviciosrestapi;
+
+import java.util.List;
+
+import  retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PostService {
+
+    String API_ROUTE = "/posts";
+    @GET(API_ROUTE)
+    Call<List<Post>> getPost();
+}
